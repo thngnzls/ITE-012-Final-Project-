672022463b744fdc683e7e0d5b9e4c3a365a0114
Brian <brianpasco1206@gmail.com>
2025-05-14 17:02:39 +0800

    feat(App): add initial railway hosting configuration

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,11 @@ import userRouter from './routes/userRoute.js'
 import productRouter from './routes/productRoute.js'
 import cartRouter from './routes/cartRoute.js'
 import orderRouter from './routes/orderRoute.js'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // App Config
 const app = express()
@@ -24,8 +29,19 @@ app.use('/api/product',productRouter)
 app.use('/api/cart',cartRouter)
 app.use('/api/order',orderRouter)
 
-app.get('/',(req,res)=>{
-    res.send("API Working")
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use('/admin', express.static(path.join(__dirname, '../admin/dist')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
 })
+
+app.get('/admin*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../admin/dist/index.html'))
+})
+
+// app.get('/',(req,res)=>{
+//     res.send("API Working")
+// })
 
 app.listen(port, ()=> console.log('Server started on PORT : '+ port))
